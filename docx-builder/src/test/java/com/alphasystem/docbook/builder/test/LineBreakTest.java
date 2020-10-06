c26ee0f2b0d35f70277bb0092ec14a55615f90c5
sfali23 <f.syed.ali@gmail.com>
2020-10-06 08:58:13 -0400

    remove line break for now

--- docx-builder/src/test/java/com/alphasystem/docbook/builder/test/LineBreakTest.java
+++ docx-builder/src/test/java/com/alphasystem/docbook/builder/test/LineBreakTest.java
@@ -13,6 +13,6 @@ public class LineBreakTest extends AbstractTest {
     @Test
     public void testLineBreak(){
         final Builder p1 = builderFactory.getBuilder(null, new Article(), -1);
-        addResult(p1, 0, 1, "Line Break Test", readXml("lineBreak", ItemizedList.class));
+        addResult(p1, 0, 2, "Line Break Test", readXml("lineBreak", ItemizedList.class));
     }
 }
