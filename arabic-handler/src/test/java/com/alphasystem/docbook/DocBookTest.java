e82bd1caa4a463d7b648b043f76a2d2349f376ab
sfali <syed.f.ali@improving.com>
2023-11-22 14:22:37 -0500

    read file from argument

--- arabic-handler/src/test/java/com/alphasystem/docbook/DocBookTest.java
+++ arabic-handler/src/test/java/com/alphasystem/docbook/DocBookTest.java
@@ -12,17 +12,29 @@ import java.nio.file.Paths;
  */
 public class DocBookTest {
 
+    /*
+        VM args: --add-opens java.base/sun.nio.ch=ALL-UNNAMED
+            --add-opens java.base/java.io=ALL-UNNAMED
+            -Dstyles=conf/styles.xml
+            -Dtemplate=conf/default.dotx -Dconf.path=.
+     */
     public static void main(String[] args) {
+        if(args == null || args.length == 0) {
+            System.err.println("File path not provided");
+            System.exit(-1);
+            return;
+        }
 
-        String path = "/Users/sfali/Documents/Arabic/GrammarLessons/Noun/noun.adoc";
+        String path = args[0];
 
         final Asciidoctor asciidoctor = Asciidoctor.Factory.create();
 
         Options options = Options.builder().backend("docbook").build();
-        asciidoctor.convertFile(Paths.get(path).toFile(), options);
+        Path srcPath = Paths.get(path);
+        asciidoctor.convertFile(srcPath.toFile(), options);
 
         try {
-            final Path destPath = DocumentBuilder.buildDocument(Paths.get(path));
+            final Path destPath = DocumentBuilder.buildDocument(srcPath);
             Desktop.getDesktop().open(destPath.toFile());
         } catch (Exception e) {
             throw new RuntimeException(e);
