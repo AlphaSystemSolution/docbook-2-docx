fd21b389477e3046f2ee8d4e7ff1fa1e1af24219
sfali <syed.f.ali@improving.com>
2023-11-23 10:35:45 -0500

    Cli module

---
+++ asciidoc-docx-cli/src/main/java/com/alphasystem/docx/cli/Main.java
@@ -0,0 +1,79 @@
+package com.alphasystem.docx.cli;
+
+import com.alphasystem.docbook.DocumentBuilder;
+import org.apache.commons.cli.*;
+
+import java.awt.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var options = new Options();
+
+        var srcOption = Option
+                .builder("s")
+                .argName("srcPath")
+                .hasArg()
+                .required()
+                .desc("Specify source Asciidoc file path")
+                .build();
+        options.addOption(srcOption);
+
+        var destOption = Option
+                .builder("d")
+                .argName("destPath")
+                .hasArg()
+                .required(false)
+                .desc("Specify destination Docx file path")
+                .build();
+        options.addOption(destOption);
+
+        // define parser
+        CommandLine cmd;
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter helper = new HelpFormatter();
+
+        try {
+            cmd = parser.parse(options, args);
+
+            Path srcPath = null;
+            if(cmd.hasOption(srcOption)) {
+                srcPath = toPath(cmd.getOptionValue(srcOption));
+                if (srcPath == null || !Files.exists(srcPath)) {
+                    printHelp("Source path does not exists", helper,options);
+                }
+            }
+
+            Path docxPath = null;
+            if(cmd.hasOption(destOption)) {
+                docxPath = toPath(cmd.getOptionValue(destOption));
+            }
+
+            Path destPath;
+            if (docxPath == null) {
+                destPath = DocumentBuilder.buildDocument(srcPath);
+            } else {
+                destPath = DocumentBuilder.buildDocument(srcPath, docxPath);
+            }
+            Desktop.getDesktop().open(destPath.toFile());
+        } catch (Exception ex) {
+            printHelp(ex.getMessage(), helper, options);
+        }
+    }
+
+    private static Path toPath(String s) {
+        if(s != null && !s.isBlank()) {
+             return Paths.get(s);
+        }
+        return null;
+    }
+
+    private static void printHelp(String s, HelpFormatter helper, Options options) {
+        System.err.println(s);
+        helper.printHelp("Usage:", options);
+        System.exit(0);
+    }
+}
---
+++ docx-cli.sh
@@ -0,0 +1,5 @@
+
+ java -Dstyles=conf/styles.xml -Dtemplate=conf/default.dotx -Dconf.path=. -jar\
+  asciidoc-docx-cli/build/libs/asciidoc-docx-cli-3.0.0-SNAPSHOT-all.jar\
+   -s /Users/sfali/Documents/Arabic/AsciiDocs/00-introduction/main.adoc\
+    -d /Users/sfali/Documents/Arabic/AsciiDocs/docx/00-introduction.docx
