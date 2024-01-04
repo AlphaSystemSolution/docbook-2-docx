3d8956a1569d11666a79608224d08d3f509d5205
sfali <syed.f.ali@improving.com>
2024-01-04 10:59:43 -0500

    updated test

--- docbook-2-docx/src/test/java/com/alphasystem/docbook/builder/test/OrderedListTest.java
+++ docbook-2-docx/src/test/java/com/alphasystem/docbook/builder/test/OrderedListTest.java
@@ -42,18 +42,12 @@ public class OrderedListTest extends AbstractTest2 {
         addHorizontalLine();
     }
 
-    /*@Test(dependsOnMethods = "testOrderedList")
-    public void testNestedOrderedList() {
-        addResult(null, 0, 5, "OrderedList Test", readXml("nested-orderedlist", OrderedList.class));
-    }
-
     @Test(dependsOnMethods = "testNestedOrderedList")
     public void testOrderedListMoreThenOnePara() {
-        final SimplePara p1 = createSimplePara(nextId(), "First paragraph of list item, this should contain numeration.");
-        final SimplePara p2 = createSimplePara(nextId(), "Second paragraph of list item, this should not contain numeration, but should be indented properly.");
-        final ListItem li1 = createListItem(nextId(), p1, p2);
-        final ListItem li2 = createListItem(nextId(), createSimplePara(null, "Second bullet point."));
-        final OrderedList orderedList = createOrderedList(nextId(), li1, li2);
-        addResult(null, 0, 3, "Ordered list with a list item having multiple para", orderedList);
-    }*/
+        addTestTitle("Nested ordered list.");
+        processContent(readXml("multi-para-ordered-list"));
+        final var content = mainDocumentPart.getContent();
+        assertEquals(content.size() - previousSize,4);
+        addHorizontalLine();
+    }
 }
