aff1339195f356acc9b2501dfa7e00036b57cc23
sfali <syed.f.ali@improving.com>
2024-01-13 20:33:37 -0500

    no need to bookmark it is already done.

--- docbook-2-docx/src/main/java/com/alphasystem/docbook/builder2/impl/block/TitleBuilder.java
+++ docbook-2-docx/src/main/java/com/alphasystem/docbook/builder2/impl/block/TitleBuilder.java
@@ -1,23 +1,12 @@
 package com.alphasystem.docbook.builder2.impl.block;
 
 import com.alphasystem.docbook.builder2.Builder;
-import com.alphasystem.openxml.builder.wml.WmlAdapter;
 import org.docbook.model.Title;
-import org.docx4j.wml.P;
-
-import java.util.List;
 
 
 public class TitleBuilder extends AbstractParaBuilder<Title> {
 
     public TitleBuilder(Title source, Builder<?> parent) {
         super(source, parent);
-    }
-
-    @Override
-    protected List<Object> doProcess(List<Object> processedChildContent) {
-        final var result = super.doProcess(processedChildContent);
-        WmlAdapter.addBookMark((P) result.get(0), getId());
-        return result;
     }
 }
