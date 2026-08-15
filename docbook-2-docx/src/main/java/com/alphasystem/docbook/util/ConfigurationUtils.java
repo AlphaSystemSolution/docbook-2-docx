3e1ffe0ed581bbc22cd1eb773a783417ad38395e
sfali23 <f.syed.ali@gmail.com>
2026-08-15 18:56:02 -0400

    add support for dynamically loaded custom styles.

--- docbook-2-docx/src/main/java/com/alphasystem/docbook/util/ConfigurationUtils.java
+++ docbook-2-docx/src/main/java/com/alphasystem/docbook/util/ConfigurationUtils.java
@@ -2,13 +2,20 @@ package com.alphasystem.docbook.util;
 
 import static java.lang.String.format;
 
+import com.alphasystem.commons.SystemException;
+import com.alphasystem.commons.util.AppUtil;
 import com.alphasystem.docbook.model.Admonition;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import io.vavr.Tuple;
 import io.vavr.Tuple2;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
-import org.apache.commons.lang3.StringUtils;
+import java.util.function.Function;
+
 import org.docbook.model.Section;
 
 /**
@@ -16,161 +23,169 @@ import org.docbook.model.Section;
  */
 public class ConfigurationUtils {
 
-  private static final ConfigurationUtils instance;
+    private static final ConfigurationUtils instance;
 
-  static {
-    instance = new ConfigurationUtils();
-  }
-
-  public static ConfigurationUtils getInstance() {
-    return instance;
-  }
-
-  private final Map<String, String> titlesMap = new HashMap<>();
-  private final Map<String, String> functionNames = new HashMap<>();
-  private final Map<Admonition, Tuple2<String, String>> admonitions = new HashMap<>();
-  private final Config mainConfig;
-  private final Config appConfig;
-  private String defaultListStyle;
-  private String tocCaption;
-  private String tableCaption;
-  private String exampleCaption;
-  private String varTermStyle;
-
-  /** Do not let anyone instantiate this class. */
-  private ConfigurationUtils() {
-    mainConfig = ConfigFactory.load();
-    appConfig = mainConfig.getConfig("docbook-docx");
-    loadTitles();
-    loadAdmonitions();
-    loadFunctionNames();
-  }
-
-  private void loadTitles() {
-    final var config = appConfig.getConfig("titles");
-    config
-        .entrySet()
-        .forEach(
-            entry -> {
-              final var key = entry.getKey();
-              final var value = entry.getValue().unwrapped().toString();
-              titlesMap.put(key, value);
-            });
-  }
-
-  private void loadAdmonitions() {
-    final var config = appConfig.getConfig("admonitions");
-    Arrays.stream(Admonition.values())
-        .forEach(
-            admonition -> {
-              final var c = config.getConfig(admonition.name().toLowerCase());
-              admonitions.put(admonition, Tuple.of(c.getString("caption"), c.getString("color")));
-            });
-  }
-
-  private void loadFunctionNames() {
-    final var config = appConfig.getConfig("script-function-names");
-    config
-        .entrySet()
-        .forEach(
-            entry -> {
-              final var key = entry.getKey();
-              final var value = entry.getValue().unwrapped().toString();
-              functionNames.put(key, value);
-            });
-  }
-
-  public Config getAppConfig() {
-    return appConfig;
-  }
-
-  public String getTitleStyle(String titleKey) {
-    return titlesMap.getOrDefault(titleKey, titlesMap.get("default"));
-  }
-
-  public String getTitleStyle(int level, Class<?> parentClass) {
-    var titleKey = parentClass.getName();
-    if (parentClass.equals(Section.class)) {
-      titleKey = format("%s.%s", titleKey, level);
+    static {
+        instance = new ConfigurationUtils();
     }
-    return getTitleStyle(titleKey);
-  }
 
-  public String getDefaultListStyle() {
-    if (Objects.isNull(defaultListStyle)) {
-      defaultListStyle = getString("list-style.default");
+    public static ConfigurationUtils getInstance() {
+        return instance;
     }
-    return defaultListStyle;
-  }
 
-  public String getVarTermStyle() {
-    if (Objects.isNull(varTermStyle)) {
-      varTermStyle = getString("list-style.var-term");
+    private final Map<String, String> titlesMap = new HashMap<>();
+    private final Map<String, String> functionNames = new HashMap<>();
+    private final Map<Admonition, Tuple2<String, String>> admonitions = new HashMap<>();
+    private final Config mainConfig;
+    private final Config appConfig;
+    private String defaultListStyle;
+    private String tocCaption;
+    private String tableCaption;
+    private String exampleCaption;
+    private String varTermStyle;
+
+    /**
+     * Do not let anyone instantiate this class.
+     */
+    private ConfigurationUtils() {
+        mainConfig = ConfigFactory.load();
+        appConfig = mainConfig.getConfig("docbook-docx");
+        loadTitles();
+        loadAdmonitions();
+        loadFunctionNames();
     }
-    return varTermStyle;
-  }
 
-  public Tuple2<String, String> getAdmonitionConfig(Admonition admonition) {
-    return admonitions.get(admonition);
-  }
-
-  public String getAdmonitionFunctionName() {
-    return functionNames.get("admonition");
-  }
-
-  public String getSideBarFunctionName() {
-    return functionNames.get("sidebar");
-  }
-
-  public String getExampleFunctionName() {
-    return functionNames.get("example");
-  }
-
-  public String getExampleCaption() {
-    if (Objects.isNull(exampleCaption)) {
-      exampleCaption = appConfig.getString("captions.example");
+    private void loadTitles() {
+        final var config = appConfig.getConfig("titles");
+        config
+                .entrySet()
+                .forEach(
+                        entry -> {
+                            final var key = entry.getKey();
+                            final var value = entry.getValue().unwrapped().toString();
+                            titlesMap.put(key, value);
+                        });
     }
-    return exampleCaption;
-  }
 
-  public String getTableCaption() {
-    if (Objects.isNull(tableCaption)) {
-      tableCaption = appConfig.getString("captions.table");
+    private void loadAdmonitions() {
+        final var config = appConfig.getConfig("admonitions");
+        Arrays.stream(Admonition.values())
+                .forEach(
+                        admonition -> {
+                            final var c = config.getConfig(admonition.name().toLowerCase());
+                            admonitions.put(admonition, Tuple.of(c.getString("caption"), c.getString("color")));
+                        });
     }
-    return tableCaption;
-  }
 
-  public String getTableOfContentCaption() {
-    if (Objects.isNull(tocCaption)) {
-      tocCaption = appConfig.getString("captions.toc");
+    private void loadFunctionNames() {
+        final var config = appConfig.getConfig("script-function-names");
+        config
+                .entrySet()
+                .forEach(
+                        entry -> {
+                            final var key = entry.getKey();
+                            final var value = entry.getValue().unwrapped().toString();
+                            functionNames.put(key, value);
+                        });
     }
-    return tocCaption;
-  }
 
-  public String getTableStyle(String ts) {
-    return getString(String.format("table.%s", ts));
-  }
-
-  public String getTemplate() {
-    var template = getString("template");
-    if (Objects.isNull(template)) {
-      template = "META-INF/docbook.dotx";
+    public Config getAppConfig() {
+        return appConfig;
     }
-    return template;
-  }
 
-  public String[] getStyles() {
-    final var defaultStyles = "META-INF/docbook-styles.xml";
-    var _styles = getString("styles");
-    _styles = StringUtils.isBlank(_styles) ? defaultStyles : defaultStyles + "," + _styles;
-    return _styles.split(",");
-  }
+    public String getTitleStyle(String titleKey) {
+        return titlesMap.getOrDefault(titleKey, titlesMap.get("default"));
+    }
 
-  public Config getConfig(String path) {
-    return mainConfig.getConfig(path);
-  }
+    public String getTitleStyle(int level, Class<?> parentClass) {
+        var titleKey = parentClass.getName();
+        if (parentClass.equals(Section.class)) {
+            titleKey = format("%s.%s", titleKey, level);
+        }
+        return getTitleStyle(titleKey);
+    }
 
-  private String getString(String key) {
-    return appConfig.hasPath(key) ? appConfig.getString(key) : null;
-  }
+    public String getDefaultListStyle() {
+        if (Objects.isNull(defaultListStyle)) {
+            defaultListStyle = getString("list-style.default");
+        }
+        return defaultListStyle;
+    }
+
+    public String getVarTermStyle() {
+        if (Objects.isNull(varTermStyle)) {
+            varTermStyle = getString("list-style.var-term");
+        }
+        return varTermStyle;
+    }
+
+    public Tuple2<String, String> getAdmonitionConfig(Admonition admonition) {
+        return admonitions.get(admonition);
+    }
+
+    public String getAdmonitionFunctionName() {
+        return functionNames.get("admonition");
+    }
+
+    public String getSideBarFunctionName() {
+        return functionNames.get("sidebar");
+    }
+
+    public String getExampleFunctionName() {
+        return functionNames.get("example");
+    }
+
+    public String getExampleCaption() {
+        if (Objects.isNull(exampleCaption)) {
+            exampleCaption = appConfig.getString("captions.example");
+        }
+        return exampleCaption;
+    }
+
+    public String getTableCaption() {
+        if (Objects.isNull(tableCaption)) {
+            tableCaption = appConfig.getString("captions.table");
+        }
+        return tableCaption;
+    }
+
+    public String getTableOfContentCaption() {
+        if (Objects.isNull(tocCaption)) {
+            tocCaption = appConfig.getString("captions.toc");
+        }
+        return tocCaption;
+    }
+
+    public String getTableStyle(String ts) {
+        return getString(String.format("table.%s", ts));
+    }
+
+    public String getTemplate() {
+        var template = getString("template");
+        if (Objects.isNull(template)) {
+            template = "META-INF/docbook.dotx";
+        }
+        return template;
+    }
+
+    public String[] getStyles() throws SystemException {
+        final var defaultStyles = "META-INF/docbook-styles.xml";
+        final var customDirPath = System.getProperty("docbook-docx.customDirPath");
+        if (customDirPath != null) {
+            final var styles = String.format("%s%s%s", customDirPath, File.separator, "styles");
+            final var consumer = (Function<Path, String>) path -> path.toAbsolutePath().toString();
+            final var customStyles = AppUtil.processDirectory(Paths.get(styles), consumer);
+            customStyles.addFirst(defaultStyles);
+            return customStyles.toArray(new String[0]);
+        }
+        return new String[] {defaultStyles};
+    }
+
+    public Config getConfig(String path) {
+        return mainConfig.getConfig(path);
+    }
+
+    private String getString(String key) {
+        return appConfig.hasPath(key) ? appConfig.getString(key) : null;
+    }
 }
